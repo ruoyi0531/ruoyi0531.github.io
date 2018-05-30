699a85dfc1bfa6ef094a895570e21e348f15bac7
ruoyi0531 <86163578@qq.com>
2018-05-30 17:07:27 +0800

    Site updated: 2018-05-30 17:07:27

--- js/photo.js
+++ js/photo.js
@@ -1,46 +1,21 @@
-define([], function () {
-    return {
-        page: 1,
-        offset: 20,
-        init: function () {
-            var that = this;
-            $.getJSON("/photo/output.json", function (data) {
-                that.render(that.page, data);
-
-                that.scroll(data);
-            });
-        },
-
-        render: function (page, data) {
-            var begin = (page - 1) * this.offset;
-            var end = page * this.offset;
-            if (begin >= data.length) return;
-            var html, li = "";
-            for (var i = begin; i < end && i < data.length; i++) {
-                li += '<li><div class="img-box">' +
-                    '<a class="img-bg" rel="example_group" href="https://github.com/ruoyi0531/zz/tree/master/baby/' + data[i] + '?raw=true"></a>' +
-                    '<img lazy-src="https://github.com/ruoyi0531/zz/tree/master/baby' + data[i] + '?raw=true" />' +
-                    '</li>';
-            }
-
-            $(".img-box-ul").append(li);
-            $(".img-box-ul").lazyload();
-            $("a[rel=example_group]").fancybox();
-        },
-
-        scroll: function (data) {
-            var that = this;
-            $(window).scroll(function() {
-                var windowPageYOffset = window.pageYOffset;
-                var windowPageYOffsetAddHeight = windowPageYOffset + window.innerHeight;
-                var sensitivity = 0;
-
-                var offsetTop = $(".instagram").offset().top + $(".instagram").height();
-
-                if (offsetTop >= windowPageYOffset && offsetTop < windowPageYOffsetAddHeight + sensitivity) {
-                    that.render(++that.page, data);
-                }
-            })
-        }
+...
+init: function () {
+    var that = this;
+    $.getJSON("../../../../source/photos/data.json", function (data) {
+        that.render(that.page, data);
+        that.scroll(data);
+    });
+},
+render: function (page, data) {
+    var begin = (page - 1) * this.offset;
+    var end = page * this.offset;
+    if (begin >= data.length) return;
+    var html, li = "";
+    for (var i = begin; i < end && i < data.length; i++) {
+        li += '<li><div class="img-box">' + '<a class="img-bg" rel="example_group" href="https://raw.githubusercontent.com/ruoyi0531/zz/master/' + data[i] + '"></a>' + '<img lazy-src="https://raw.githubusercontent.com/ruoyi0531/zz/master/' + data[i] + '" />' + '</li>';
     }
-})
+    $(".img-box-ul").append(li);
+    $(".img-box-ul").lazyload();
+    $("a[rel=example_group]").fancybox();
+},
+...
--- photos/ins.js
+++ photos/ins.js
@@ -116,8 +116,8 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'https://raw.githubusercontent.com/ruoyi0531/zz/master/baby' + data.link[i] + '.min.jpg';
-          var src = 'https://raw.githubusercontent.com/ruoyi0531/zz/master/baby' + data.link[i];
+          var minSrc = 'https://raw.githubusercontent.com/ruoyi0531/zz/master/min_photos/' + data.link[i] + '.min.jpg';
+          var src = 'https://raw.githubusercontent.com/ruoyi0531/zz/master/photos/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
           src += '.jpg';
